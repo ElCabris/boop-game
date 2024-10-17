bd22a23f6387b16137c585af92a3d60c223a25ff
Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
2024-10-17 18:04:23 -0500

    feat: encapsulation change of the is_game_over
    moves from private to public

--- include/board.hpp
+++ include/board.hpp
@@ -7,16 +7,17 @@ namespace boop {
 struct Position {
   int row, col;
 };
+
 class Board {
 private:
   NodeMatrix<Piece> _matrix;
-  bool is_game_over();
 
 public:
   Board(const std::size_t &size);
   void add_piece(Position &position, PieceType type);
   Node<Piece> *get_piece(Position position) const;
   std::size_t get_size() const;
+  bool is_game_over();
 };
 } // namespace boop
 #endif
